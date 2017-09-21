9ba81c7ef9f3421ab5b125bf71e844c3ccf7e941
talmeym <mtalmey@gmail.com>
2017-09-21 11:12:33 +0100

    renamed FileResponse CreateFileResponse

--- src/main/java/com/emarte/regurgitator/extensions/web/CreateFileResponseJsonLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/web/CreateFileResponseJsonLoader.java
@@ -9,17 +9,17 @@ import static com.emarte.regurgitator.core.CoreConfigConstants.SOURCE;
 import static com.emarte.regurgitator.core.JsonConfigUtil.*;
 import static com.emarte.regurgitator.extensions.web.WebConfigConstants.PATH_PREFIX;
 
-public class FileResponseJsonLoader implements JsonLoader<FileResponse> {
-	private static Log log = Log.getLog(FileResponseJsonLoader.class);
+public class CreateFileResponseJsonLoader implements JsonLoader<CreateFileResponse> {
+	private static Log log = Log.getLog(CreateFileResponseJsonLoader.class);
 
 	@Override
-	public FileResponse load(JSONObject jsonObject, Set<Object> allIds) throws RegurgitatorException {
+	public CreateFileResponse load(JSONObject jsonObject, Set<Object> allIds) throws RegurgitatorException {
 		String id = loadId(jsonObject, allIds);
 		String source = loadMandatoryStr(jsonObject, SOURCE);
 		String pathPrefix = loadOptionalStr(jsonObject, PATH_PREFIX);
 
 		ContextLocation location = source != null ? new ContextLocation(source) : null;
 		log.debug("Loaded file response '" + id + "'");
-		return new FileResponse(id, new ValueSource(location, null), pathPrefix);
+		return new CreateFileResponse(id, new ValueSource(location, null), pathPrefix);
 	}
 }
--- src/main/java/com/emarte/regurgitator/extensions/web/ExtensionsWebJsonPackageMap.java
+++ src/main/java/com/emarte/regurgitator/extensions/web/ExtensionsWebJsonPackageMap.java
@@ -5,7 +5,7 @@ import com.emarte.regurgitator.core.AbstractJsonPackageMap;
 import java.util.*;
 
 public class ExtensionsWebJsonPackageMap extends AbstractJsonPackageMap {
-	private static final List<String> kinds = Arrays.asList("http-call", "query-param-processor", "create-http-response");
+	private static final List<String> kinds = Arrays.asList("http-call", "query-param-processor", "create-http-response", "create-file-response");
 
 	public ExtensionsWebJsonPackageMap() {
 		addPackageMapping(kinds, "com.emarte.regurgitator.extensions.web");
